d15b2fcb181dfd312fb252ba52266b93e519a599
arke0909 <arke0909@gmail.com>
2025-09-19 09:36:00 +0900

    Feat : WeatherEvents

---
+++ Assets/01.Scripts/Events/WeatherEvents.cs
@@ -0,0 +1,22 @@
+﻿using Code.Weathers;
+using Core.GameEvent;
+
+namespace Code.Events
+{
+    public static class WeatherEvents
+    {
+        public static WeatherChangeEvent WeatherChangeEvent = new WeatherChangeEvent();
+    }
+
+    public class WeatherChangeEvent : GameEvent
+    {
+        public WeatherDataSO data;
+
+        public WeatherChangeEvent Init(WeatherDataSO data)
+        {
+            this.data = data;
+            return this;
+        }
+        
+    }
+}
