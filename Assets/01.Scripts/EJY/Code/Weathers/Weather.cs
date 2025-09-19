d50a2810e36ea24df32869bd048ea139f5d868d7
arke0909 <arke0909@gmail.com>
2025-09-19 09:35:35 +0900

    Fix : Weather & WeatherDataSO

--- Assets/01.Scripts/EJY/Code/Weathers/Weather.cs
+++ Assets/01.Scripts/EJY/Code/Weathers/Weather.cs
@@ -1,3 +1,5 @@
+using Code.Events;
+using Core.GameEvent;
 using UnityEngine;
 
 namespace Code.Weathers
@@ -5,10 +7,15 @@ namespace Code.Weathers
     public class Weather : MonoBehaviour
     {
         [SerializeField] private WeatherDataSO[] weatherData;
+        [SerializeField] private GameEventChannelSO weatherChannel;
+        
+        public WeatherType CurrentWeather { get; private set; } = WeatherType.Normal;
 
         private void ChangeWeather()
         {
+            WeatherDataSO data = weatherData[Random.Range(0, weatherData.Length)];
             
+            weatherChannel.RaiseEvent(WeatherEvents.WeatherChangeEvent.Init(data));
         }
     }
 }
--- Assets/01.Scripts/EJY/Code/Weathers/WeatherDataSO.cs
+++ Assets/01.Scripts/EJY/Code/Weathers/WeatherDataSO.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 namespace Code.Weathers
@@ -6,14 +5,7 @@ namespace Code.Weathers
     [CreateAssetMenu(fileName = "WeatherData", menuName = "SO/Weather/Data", order = 0)]
     public class WeatherDataSO : ScriptableObject
     {
-        public string weatherName;
-        public int nameHash;
+        public WeatherType weatherType;
         public Sprite weatherIcon;
-
-        private void OnValidate()
-        {
-            if(string.IsNullOrEmpty(weatherName)) return;
-            nameHash = Animator.StringToHash(weatherName);
-        }
     }
 }
