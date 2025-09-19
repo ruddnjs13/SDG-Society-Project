3d41cea3bcffa67f2591a4bcc7eee7bc57444064
arke0909 <arke0909@gmail.com>
2025-09-19 09:36:15 +0900

    Feat : WeatherExtended

---
+++ Assets/01.Scripts/EJY/Code/Weathers/Utility/WeatherExtended.cs
@@ -0,0 +1,10 @@
+﻿namespace Code.Weathers.Utility
+{
+    public static class WeatherExtended
+    {
+        public static bool CanActionByWeather(this WeatherDataSO data, WeatherType powerStationWeatherType)
+        {
+            return data.weatherType == powerStationWeatherType;
+        }
+    }
+}
