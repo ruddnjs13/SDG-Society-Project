8c8e1759c828cce0e3768f3226dc5a0598ba4af0
arke0909 <arke0909@gmail.com>
2025-09-22 17:04:58 +0900

    Feat : WeatherParticleController

---
+++ Assets/01.Scripts/EJY/Code/Weathers/WeatherParticleController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Code.Events;
+using Core.GameEvent;
+using UnityEngine;
+
+namespace Code.Weathers
+{
+    public class WeatherParticleController : MonoBehaviour
+    {
+        [SerializeField] private GameEventChannelSO environmentChannel;
+        private Dictionary<WeatherType, WeatherParticle> _weatherParticles = new Dictionary<WeatherType, WeatherParticle>();
+        private WeatherParticle _currentWeatherParticle;
+        
+        private void Awake()
+        {
+            foreach (var weatherParticle in GetComponentsInChildren<WeatherParticle>())
+            {
+                _weatherParticles.Add(weatherParticle.WeatherType, weatherParticle);
+            }
+            
+            environmentChannel.AddListener<WeatherChangeEvent>(HandleWeatherChange);
+        }
+
+        private void HandleWeatherChange(WeatherChangeEvent evt)
+        {
+            _currentWeatherParticle?.StopParticle();
+            
+            WeatherType type = WeatherType.None;
+            foreach (WeatherType flag in Enum.GetValues(typeof(WeatherType)))
+            {
+                if (((WeatherType)evt.data.TypeBit & flag) == flag && flag != WeatherType.None)
+                {
+                    type = flag;
+                    break;
+                }
+            }
+            
+            if(type == WeatherType.None) return;
+            
+            _currentWeatherParticle = _weatherParticles.GetValueOrDefault(type);
+            Debug.Assert(_currentWeatherParticle != null, $"Weather particle is null,type is {type}");
+            
+            _currentWeatherParticle.PlayParticle();
+        }
+    }
+}
