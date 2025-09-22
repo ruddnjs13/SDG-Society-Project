926ca19cfbfecdfac4730d9463269e15c0b291d6
pormer <juwonst@gmail.com>
2025-09-22 14:18:47 +0900

    feat: add sorting layer

--- Assets/RuddnjsLib/Core/MonoSingleton.cs
+++ Assets/RuddnjsLib/Core/MonoSingleton.cs
@@ -16,7 +16,7 @@ public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
 
             if (_instance == null)
             {
-                _instance = GameObject.FindObjectOfType<T>();
+                _instance = FindFirstObjectByType<T>();
 
                 if (_instance == null)
                     Debug.LogError($"{typeof(T).Name} singleton is not exist");
