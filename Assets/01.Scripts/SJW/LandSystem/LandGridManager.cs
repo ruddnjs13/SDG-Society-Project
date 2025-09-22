971695952320e7a52991ee9aaf81d8b921b4a11d
pormer <juwonst@gmail.com>
2025-09-22 17:48:12 +0900

    Revert "feat: item random spawn"
    This reverts commit 01d2bca11eabe191e314cfde0ee8bf8fd381948a.

--- Assets/01.Scripts/SJW/LandSystem/LandGridManager.cs
+++ Assets/01.Scripts/SJW/LandSystem/LandGridManager.cs
@@ -1,6 +1,4 @@
 ﻿using System.Collections.Generic;
-using _01.Scripts.SJW.Events;
-using Core.GameEvent;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -8,16 +6,15 @@ namespace LandSystem
 {
     public class LandGridManager : MonoBehaviour
     {
-        [SerializeField] private GameEventChannelSO landChannel;
         [SerializeField] private Tilemap canBuildingMap;
 
-        private Dictionary<Vector2Int, Transform> _powerStationGrid;
+        private Dictionary<Vector2Int, GameObject> _powerStationGrid;
         private HashSet<Vector2Int> _grid; //설치 가능한 부분인지 확인
         
         private void Awake()
         {
             _grid = new HashSet<Vector2Int>();
-            _powerStationGrid = new Dictionary<Vector2Int, Transform>();
+            _powerStationGrid = new Dictionary<Vector2Int, GameObject>();
             
             foreach (var pos in canBuildingMap.cellBounds.allPositionsWithin)
             {
@@ -27,41 +24,9 @@ namespace LandSystem
             }
         }
 
-        public bool IsPossibleBuild(BuildingData data)
+        public bool IsPossibleBuild(Vector2Int pos)
         {
-            foreach (var pos in data.OffsetPosListList)
-            {
-                Vector2Int position = data.PivotPos + pos;
-                if (!_grid.Contains(position) || _powerStationGrid[position])
-                {
-                    return false;
-                }
-            }
-            
-            return true;
-        }
-
-        public void SetBuildStation(BuildingData data)
-        {
-            if (!IsPossibleBuild(data))
-            {
-                var evt = LandEvents.BuildFailEvent.Initializer(data.PivotPos);
-                landChannel.RaiseEvent(evt);
-                return;
-            }
-
-            foreach (var offset in data.OffsetPosListList)
-            {
-                Vector2Int pos = data.PivotPos + offset;
-
-                _powerStationGrid[pos] = data.Target;
-            }
-            
-            if(!IsPossibleBuild(data))
-            {
-                var evt = LandEvents.BuildCompleteEvent.Initializer(data);
-                landChannel.RaiseEvent(evt);
-            }
+            return _grid.Contains(pos) && _powerStationGrid.GetValueOrDefault(pos) == null;
         }
     }
 }
