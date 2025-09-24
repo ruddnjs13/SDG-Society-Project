3995760077412c166d2da7bdfea959e4c476d471
arke0909 <arke0909@gmail.com>
2025-09-24 19:44:17 +0900

    Feat : Normal Effect

---
+++ Assets/01.Scripts/EJY/Code/Weathers/NormalEffect.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Code.Weathers
+{
+    public class NormalEffect : WeatherEffect
+    {
+        public override void PlayEffect()
+        { }
+
+        public override void StopEffect()
+        { }
+    }
+}
