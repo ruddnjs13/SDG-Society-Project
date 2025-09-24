fef6426d1bc3510d315c58384dec164024fd6609
arke0909 <arke0909@gmail.com>
2025-09-24 17:50:14 +0900

    Feat : RainyEffect

---
+++ Assets/01.Scripts/EJY/Code/Weathers/RainyEffect.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Code.Weathers
+{
+    public class RainyEffect : WeatherEffect
+    {
+        [SerializeField] private ParticleSystem rainyEffect;
+        
+        public override void PlayEffect()
+        {
+            rainyEffect.Play();
+        }
+
+        public override void StopEffect()
+        {
+            rainyEffect.Stop();
+        }
+    }
+}
