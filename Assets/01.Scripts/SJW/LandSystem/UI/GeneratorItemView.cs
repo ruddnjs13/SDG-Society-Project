6c13d8631fedb2247eccbd4d124f4bc9715edcef
pormer <juwonst@gmail.com>
2025-09-27 20:51:46 +0900

    feat: generatotr ui item

---
+++ Assets/01.Scripts/SJW/LandSystem/UI/GeneratorItemView.cs
@@ -0,0 +1,26 @@
+﻿using Core.GameEvent;
+using LKW.Generators;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace LandSystem.UI
+{
+    public class GeneratorItemView : MonoBehaviour
+    {
+        [SerializeField] private Image iconImage;
+        [SerializeField] private GameEventChannelSO pointChannel;
+        [SerializeField] private TextMeshProUGUI generatorNameText;
+        [SerializeField] private TextMeshProUGUI generatorValueText;
+        [SerializeField] private Button buyButton;
+        
+        public void Initialize(GeneratorDataSO data)
+        {
+            buyButton.onClick.AddListener(HandleRequestBuy);
+        }
+
+        private void HandleRequestBuy()
+        {
+        }
+    }
+}
