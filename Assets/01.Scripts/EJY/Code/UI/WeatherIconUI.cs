3d007ae5ee1a888bed3f7d63955dc2e299303f74
arke0909 <arke0909@gmail.com>
2025-09-28 17:58:43 +0900

    Feat : WeatherUI

---
+++ Assets/01.Scripts/EJY/Code/UI/WeatherIconUI.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Code.Events;
+using Core.GameEvent;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Code.UI
+{
+    public class WeatherIconUI : MonoBehaviour
+    {
+        [SerializeField] private GameEventChannelSO environmentChannel;
+        [SerializeField] private Image weatherIcon;
+
+        private void Awake()
+        {
+            environmentChannel.AddListener<EnvironmentChangeEvent>(HandleEnvironmentChange);
+        }
+
+        private void HandleEnvironmentChange(EnvironmentChangeEvent evt)
+        {
+            weatherIcon.sprite = evt.icon;
+        }
+    }
+}
