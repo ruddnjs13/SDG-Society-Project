680eeaccebf7a757cc5f18b814ee1a0c70b6747d
ruddnjs13 <ruddnjs013@gmail.com>
2025-09-29 08:42:40 +0900

    chore

---
+++ Assets/01.Scripts/LKW/Events/PollutionEvents.cs
@@ -0,0 +1,21 @@
+﻿using Core.GameEvent;
+
+namespace LKW.Generaters.LKW.Events
+{
+    public static class PollutionEvents
+    {
+        public static GetPollutionEvent GetPollutionEvent = new GetPollutionEvent();
+    }
+
+    public class GetPollutionEvent : GameEvent
+    {
+        public int pollutionAmount;
+
+        public GetPollutionEvent Initializer(int pollutionAmount)
+        {
+            this.pollutionAmount = pollutionAmount;
+            
+            return this;
+        }
+    }
+}
---
+++ Assets/01.Scripts/LKW/Penalties/PollutionManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Core.GameEvent;
+using LKW.Generaters.LKW.Events;
+using UnityEngine;
+
+namespace LKW.Penalties
+{
+    public class PollutionManager : MonoBehaviour
+    {
+        [SerializeField] private GameEventChannelSO pollutionChannel;
+        
+        [SerializeField] private int maxPollution;
+
+        public int currentPollution = 0;
+
+        private void OnEnable()
+        {
+            pollutionChannel.AddListener<GetPollutionEvent>(HandlePollution);
+        }
+        private void OnDestroy()
+        {
+            pollutionChannel.RemoveListener<GetPollutionEvent>(HandlePollution);
+        }
+
+        private void HandlePollution(GetPollutionEvent evt)
+        {
+            currentPollution += evt.pollutionAmount;
+
+            if (currentPollution >= maxPollution)
+            {
+                
+            }
+        }
+    }
+}
