d5bd476f56185762a0dd2d6d6761396639c146eb
arke0909 <arke0909@gmail.com>
2025-09-29 09:17:32 +0900

    Feat : ButtonEvents & FadeEventSender

---
+++ Assets/01.Scripts/EJY/Code/FadeSystem/FadeEventSender.cs
@@ -0,0 +1,15 @@
+﻿using Code.Events;
+using Core.GameEvent;
+using UnityEngine;
+
+namespace Code.UI.FadeSystem
+{
+    public class FadeEventSender : MonoBehaviour
+    {
+        [SerializeField] private GameEventChannelSO sceneChannel;
+
+        public void SendFadeInEvent(string sceneName) =>
+            sceneChannel.RaiseEvent(SceneEvents.FadeEvent.Init(true, sceneName));
+        public void SendFadeOutEvent() => sceneChannel.RaiseEvent(SceneEvents.FadeEvent.Init(false, string.Empty));
+    }
+}
