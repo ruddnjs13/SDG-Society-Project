49eb8cf38c0347f2c5a9b43cae6a2d42bce2e11a
arke0909 <arke0909@gmail.com>
2025-09-30 00:11:05 +0900

    Feat : TimerUI

---
+++ Assets/01.Scripts/EJY/Code/UI/TimerUI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace Code.UI
+{
+    public class TimerUI : MonoBehaviour
+    {
+        [SerializeField] private float limitTime = 1200;
+        [SerializeField] private Image barImage;
+        private  float _currentTime;
+
+        public UnityEvent<bool> OnFailEvent;
+
+        private void Awake()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Update()
+        {
+            _currentTime += Time.deltaTime;
+            float ratio = _currentTime / limitTime;
+
+            if (_currentTime >= limitTime)
+            {
+                OnFailEvent?.Invoke(false);
+            }
+        }
+    }
+}
