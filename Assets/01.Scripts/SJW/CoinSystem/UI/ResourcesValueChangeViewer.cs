6f4f3c9f2558cdc19ae6515e32e6d19460ea87bf
pormer <juwonst@gmail.com>
2025-09-29 09:42:18 +0900

    fix: resources text animation

--- Assets/01.Scripts/SJW/CoinSystem/UI/ResourcesValueChangeViewer.cs
+++ Assets/01.Scripts/SJW/CoinSystem/UI/ResourcesValueChangeViewer.cs
@@ -9,15 +9,30 @@ namespace CoinSystem.UI
     {
         [SerializeField] private string targetResources;
         [SerializeField] private TextMeshProUGUI currentValueText;
+        [SerializeField] private Color highlightColor;
+        [SerializeField] private Color rowColor;
 
-        public void HandleChangeValue(int current, int prev)
+        public void HandleChangeValue(int current, int prev, bool isDirect)
         {
             var value = prev;
-            DOTween.To (() =>value, x =>
+
+            if (isDirect)
+            {
+                currentValueText.SetText(current.ToString());
+                return;
+            }
+            
+            currentValueText.DOColor(current > prev ? highlightColor : rowColor, 0.1f);
+            currentValueText.transform.DOScale(1.35f, 0.2f).SetEase(Ease.OutCubic);
+            DOTween.To(() => value, x =>
             {
                 value = x;
                 currentValueText.SetText(x.ToString());
-            }, current, 0.1f);
+            }, current, 0.2f).OnComplete(() =>
+            {
+                currentValueText.DOColor(Color.white, 0.1f);
+                currentValueText.transform.DOScale(1f, 0.2f).SetEase(Ease.InQuart);
+            });
         }
     }
 }
