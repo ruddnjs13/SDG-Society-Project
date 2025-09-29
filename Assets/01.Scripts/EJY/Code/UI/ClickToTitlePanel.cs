c5b0a01297a5a27e014ea9241cc2154534fd37cb
arke0909 <arke0909@gmail.com>
2025-09-29 22:36:37 +0900

    Feat : GameClear & GameOver

---
+++ Assets/01.Scripts/EJY/Code/UI/ClickToTitlePanel.cs
@@ -0,0 +1,27 @@
+﻿using Code.Events;
+using Core.GameEvent;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Code.UI
+{
+    public class ClickToTitlePanel : MajorUI, IPointerClickHandler
+    {
+        [SerializeField] private GameEventChannelSO sceneChannel;
+        [SerializeField] private TextMeshProUGUI clickText;
+        [SerializeField] private float fadeDuration = 0.2f;
+        protected override void Awake()
+        {
+            base.Awake();
+            Open();
+            clickText.DOFade(0, fadeDuration).SetLoops(-1, LoopType.Yoyo);
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            sceneChannel.RaiseEvent(SceneEvents.FadeEvent.Init(true, "TitleScene"));
+        }
+    }
+}
